98fee752a631aae98a07e487d6e30da379ac21ff
MoamenAttia <MoamenAttia@outlook.com>
2018-12-15 20:01:35 +0200

    flag register

--- Model Sim/flagRegister.vhd
+++ Model Sim/flagRegister.vhd
@@ -7,7 +7,7 @@ USE IEEE.std_logic_1164.all;
 
 ENTITY flagReg IS
   generic( n : integer := 16 );
-  PORT( clk , Rst , En , C : IN  std_logic;
+  PORT( clk , Rst , En , carryOut : IN  std_logic;
         A , B , C    : IN  std_logic_vector(n-1 DOWNTO 0);
         q            : OUT std_logic_vector(n-1 DOWNTO 0));
 END flagReg;
@@ -19,9 +19,13 @@ architecture a_flagReg of flagReg is
         if rst = '1' then
           q <="0000";
         elsif rising_edge(clk) and rst = '0' and En = '1' then
-          q(0) <= C;
+          q(0) <= carryOut;
           q(1) <= ( not(A(n-1)) and not(B(n-1)) and C(n-1) ) or ( A(n-1) and B(n-1) and not(C(n-1)) );
-          q(2) <= '0' when Rst = '0' and C = (others => '0) else '1' when Rst = '0';
+          if rst = '0' and  C = X"0000" then
+              q(2) <= '1';
+          elsif rst = '0' then
+              q(2) <= '0';
+          end if;
           q(3) <= C(n-1);
           q(n-1 downto 4) <= (others => '0');
         end if;
